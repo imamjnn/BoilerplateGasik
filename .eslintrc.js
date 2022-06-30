82d17e7d334443fa2e3d8812e6aee057e5c1215c
Imam Jinani <imamjinani@Imams-MacBook-Air.local>
2022-06-30 08:56:26 +0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+
+        // eslint-plugin-prettier
+        'prettier/prettier': 2,
+      },
+    },
+  ],
+};
